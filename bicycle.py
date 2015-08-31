69914b69329975a180a2bd45779be5f680e4bb4f
lpstandard <lps.tony@gmail.com>
2015-08-31 19:08:26 +0000

    Initial commit

---
+++ bicycle.py
@@ -0,0 +1,26 @@
+class bicycle(object):
+    def __init__(self, model name, weight, cost):
+        self.name = name
+        self.weight = weight
+        self.cost = cost
+
+class bike_shops(object):
+    def __init__(self, inventory):
+        self.inventory = inventory
+    def margin(self):
+        return cost*(1+20%)
+
+class customers(object):
+    def __init__(self, budget):
+        self.budget= budget
+    def buy(self, bike):
+        self.bike = bike
+
+Bike sport shop = bike_shops(inventory{'XT':200,'MTN':500, 'ROAD':700,'TT': 1500,'COMMUNTE':100,'TRI':5000}
+    )
+Abby = customers(200)
+Bob = customers(500)
+Cate = customers(1000)
+
+print (Bike sport shop)
+print ()
